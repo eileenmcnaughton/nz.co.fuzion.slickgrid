37f06e00f53cca29abfb433c8fefffb199fa9993
Eileen <eileen@fuzion.co.nz>
2013-10-02 04:41:19 +1300

    Add basic overview page ( which now looks like it should be combined with our 'add batch page')

--- CRM/Slickgrid/Page/CiviSlick.php
+++ CRM/Slickgrid/Page/CiviSlick.php
@@ -2,4 +2,16 @@
 
 class CRM_Slickgrid_Page_CiviSlick extends CRM_Core_Page {
 
+ function run() {
+  $batches = civicrm_api3('SlickBatch', 'get', array());
+  foreach ($batches['values'] as &$batch) {
+    $batch['profile'] = civicrm_api3('uf_group', 'getvalue', array(
+      'return' => 'title',
+      'id' => $batch['profile_id'])
+    );
+    $batch['url'] = CRM_Utils_System::url('civicrm/civislick/grid', "reset=1&gridid={$batch['id']}");
+  }
+  $this->assign('batches', $batches['values']);
+  parent::run();
+ }
 }
--- templates/CRM/Slickgrid/Page/CiviSlick.tpl
+++ templates/CRM/Slickgrid/Page/CiviSlick.tpl
@@ -3,3 +3,9 @@
 <p>Here we will put a list of batches in progress & possibly the ability to print summaries
 of completed ones - esp for deposit slips</p>
 
+<table>
+ {foreach from=$batches key=id item=batch}
+  <tr><td><a href ='{$batch.url}'>{$batch.profile}</a></td></tr>
+ {/foreach}
+</table>
+
